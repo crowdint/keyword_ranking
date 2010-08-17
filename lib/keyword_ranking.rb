0d95fe7e42540e5b972e10b68f3da7873c6cb021
LuisVM <whizkas@hotmail.com>
2010-08-17 13:16:47 -0500

    Added extract_options! function to Array class

--- lib/keyword_ranking.rb
+++ lib/keyword_ranking.rb
@@ -1,16 +1,23 @@
 require 'singleton'
+require 'nokogiri'
+
+class Array
+  def extract_options!
+    last.is_a?(::Hash) ? pop : {}
+  end
+end
 
 module KeywordRanking
   class Ranking
 
     RES_LIMIT = 200
     RES_PER_PAGE = 10 #careful, it doesn't work with more than 10 for all search engines!
-    
+
     include Singleton
 
     def get(*args) # keyword, url, engine, limit
       options = args.extract_options!
-      limit = options[:limit].present? ? options[:limit].to_i : RES_LIMIT
+      limit = options[:limit] ? options[:limit].to_i : RES_LIMIT
       site_uri = options[:url].gsub(/(http:\/\/|https:\/\/)/, '').gsub(/^www/, '')
       validate_arguments options
       find_ranking(options[:keyword], site_uri, limit, options[:engine])
@@ -19,9 +26,9 @@ module KeywordRanking
     protected
 
     def validate_arguments(options)
-      raise "Keyword and site parameters must be Strings" unless options[:keyword].present? and options[:keyword].is_a?(String) and options[:url].present? and options[:url].is_a?(String)
+      raise "Keyword and site parameters must be Strings" unless options[:keyword].is_a?(String) and options[:url].is_a?(String)
       raise "Limit of #{RES_LIMIT} results at most" if options[:limit] > RES_LIMIT
-      raise "Engine should be 'bing', 'google' or 'yahoo'" unless options[:engine].present? and [:bing, :google, :yahoo].include?(options[:engine].to_sym)
+      raise "Engine should be 'bing', 'google' or 'yahoo'" unless [:bing, :google, :yahoo].include?(options[:engine].to_sym)
     end
 
     def find_ranking(keyword, site, limit, engine)
@@ -50,3 +57,4 @@ module KeywordRanking
 
   end
 end
+
